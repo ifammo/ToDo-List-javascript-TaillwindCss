eca84a59bd200d14013ec39e7c767a046137c587
ifammo <ifammo99@gmail.com>
2023-11-15 17:29:51 +0100

    Activate button delete

--- maim.js
+++ maim.js
@@ -3,8 +3,8 @@ function customTask(message){
     return ` <div class="flex justify-between mt-5  " >
     <!--Single Tasks-->
     <div class="flex items-center gap-2">
-        <button class=" w-8 bg-slate-200 rounded-full p-1 ">
-            <i class="fa-solid fa-check"></i>
+        <button class=" w-8 bg-slate-200 hover:bg-cyan-300 rounded-full p-1 ">
+            <i class="fa-solid fa-check "></i>
         </button>
         
         <h2>${message}</h2>
@@ -13,7 +13,7 @@ function customTask(message){
         <button class="text-slate-500 hover:bg-slate-500 hover:text-white w-10 rounded-full p-2 border">
             <i class="fa-solid fa-pen"></i>
         </button>
-        <button class="text-slate-500 hover:bg-slate-500 hover:text-white w-10 rounded-full p-2 border">
+        <button onclick="deleteTask(this)" class="text-slate-500 hover:bg-slate-500 hover:text-white w-10 rounded-full p-2 border">
             <i class="fa-solid fa-trash"></i>
         </button>
     </div>`
@@ -28,4 +28,7 @@ function addTask() {
     input.value=""
     }
    
+}
+function deleteTask(target){
+    target.parentElement.parentElement.remove()
 }
